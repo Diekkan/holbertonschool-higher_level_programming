d28cd6fae3948cc7adb93044629974cb10e807d1
Diekkan <merentiel.diego@gmail.com>
2021-12-23 08:13:40 -0800

    a program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    elif len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    if len(sys.argv) >= 2:
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
+            i += 1
