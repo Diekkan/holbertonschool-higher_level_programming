7bb0c3419cb7c044eae06fc91e6f1d499923355c
Diekkan <merentiel.diego@gmail.com>
2022-01-16 22:28:58 -0800

    added first

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for element in my_list:
+        try:
+            if count < x:
+                print("{}".format(element, end=''))
+                count += 1
+        except IndexError:
+            break
+    return (count)
