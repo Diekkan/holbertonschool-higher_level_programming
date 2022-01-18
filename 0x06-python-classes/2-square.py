271ad82fcff97ab704066ecf347f22a6d99e42c9
Diekkan <merentiel.diego@gmail.com>
2022-01-18 12:20:25 -0800

    ex 2 added

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,15 +5,14 @@ Attributes:
 
 
 class Square:
-    """Class name: square"""
+    """Class name: Square"""
     def __init__(self, size=0):
         """init method
         Args:
-            size: size of square
+            size=0: size of square
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-            
