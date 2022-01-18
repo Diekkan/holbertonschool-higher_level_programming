1c39762839e9688cf2cf024345db1dce9a28a5e6
Diekkan <merentiel.diego@gmail.com>
2022-01-18 13:13:39 -0800

    ex 4 added

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,3 +22,15 @@ class Square:
 
     def area(self):
         return self.__size ** 2
+    """gets size"""
+    @property
+    def size(self):
+        return self.__size
+    """sets size"""
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
