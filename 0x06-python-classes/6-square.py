4adfa045d85c3b7f353e222a2a7f2907cd83ae2c
Diekkan <merentiel.diego@gmail.com>
2022-01-18 14:12:07 -0800

    ex 6 added, position handled

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,13 +18,13 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-	if not isinstance(value, tuple):
+        if not isinstance(position, tuple):
             raise TypeError('position must be a tuple of 2 positive integers')
-        if not isinstance(value[0], int) or not isinstance(value[1], int):
+        if not isinstance(position[0], int) or not isinstance(position[1], int):
             raise TypeError('position must be a tuple of 2 positive integers')
-        if value[0] < 0 or value[1] < 0:
+        if position[0] < 0 or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) != 2:
+        if len(position) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__size = size
         self.__position = position
@@ -50,7 +50,7 @@ class Square:
     """sets position"""
     @position.setter
     def position(self, value):
-	if not isinstance(value, tuple):
+        if not isinstance(value, tuple):
             raise TypeError('position must be a tuple of 2 positive integers')
         if not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError('position must be a tuple of 2 positive integers')
