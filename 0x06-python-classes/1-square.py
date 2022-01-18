e1488c404ffe91e9600e3021a66c0109c4a6218b
Diekkan <merentiel.diego@gmail.com>
2022-01-18 12:10:03 -0800

    ex 1 added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""0. My first square
+Attributes:
+"""
+
+
+class Square:
+    """Class name: square"""
+    def __init__(self, size):
+        """init method
+        Args:
+            size: size of square
+        """
+        self.__size = size
