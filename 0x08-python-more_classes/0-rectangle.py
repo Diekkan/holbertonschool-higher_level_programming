b87f3d143a741cbd41ae25a56924b2ecb8ce6f88
Diekkan <merentiel.diego@gmail.com>
2022-01-24 15:38:17 -0800

    First two ex

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# A rectangle class...
+class Rectangle:
+    # The rectangle class.
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# A rectangle class...
+class Rectangle:
+    # The rectangle class.
+    def __init__(self, width=0, height=0):
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >=0")
+        self.__width = width
+        self.__height = height
+
+    # Returns height.
+    @property
+    def height(self):
+        return self.__height
+    # Returns width.
+    @property
+    def width(self):
+        return self.__width
+    # Sets height.
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    # Sets width.
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
