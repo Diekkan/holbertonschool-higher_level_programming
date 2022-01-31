ad3e229d21df40a0edb7eee7210c28b82be5a739
root <root@DESKTOP-PN1Q2OU.localdomain>
2022-01-31 20:24:14 -0300

    fixed 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,6 +19,14 @@ class BaseGeometry:
 
 class Rectangle(BaseGeometry):
     """ Rectangle class, BaseGeometry subclass."""
+    def integer_validator(self, name, value):
+        """ validates value """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
     def __init__(self, width, height):
         """creating an instance """
         self.integer_validator("width", width)
