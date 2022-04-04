22c155376cf2f4668410c8347d2048cc7dec2bc3
Diekkan <merentiel.diego@gmail.com>
2022-04-04 10:14:06 -0700

    Write a script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (process.argv[2]) {
+  if (parseInt(process.argv[2]) > 0) {
+    for (let i = 0; i < parseInt(process.argv[2]); i++) {
+      console.log('C is fun');
+    }
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
