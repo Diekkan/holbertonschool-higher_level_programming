9934a1ef9bf37c63bc648e304bd945a0ccba54d0
Diekkan <merentiel.diego@gmail.com>
2022-04-04 08:24:31 -0700

    First 3 ex

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+if (process.argv[2]) {
+  if (process.argv[3]) {
+    console.log('Arguments found');
+  } else {
+    console.log('Argument found');
+  }
+} else {
+  console.log('No argument');
+}
