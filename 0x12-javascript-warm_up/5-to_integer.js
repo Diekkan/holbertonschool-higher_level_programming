561016c434cbca7b3a9f57ce8b4f9a92005a92b7
Diekkan <merentiel.diego@gmail.com>
2022-04-04 09:34:35 -0700

    a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,9 +2,9 @@
 
 if (process.argv[2]) {
   if (parseInt(process.argv[2])) {
-  console.log('My number: '  + parseInt(process.argv[2]));
+    console.log('My number: ' + parseInt(process.argv[2]));
   } else {
-  console.log('Not a number');
+    console.log('Not a number');
   }
 } else {
   console.log('Not a number');
