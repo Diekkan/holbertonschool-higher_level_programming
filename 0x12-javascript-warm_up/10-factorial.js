bdbca18b019513f1463f4f8e7125264a94913d96
Diekkan <merentiel.diego@gmail.com>
2022-04-04 11:32:26 -0700

    a script that searches the second biggest integer in the list of arguments.

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorialize (num) {
+  if (isNaN(num)) {
+    return (1);
+  } else if (num < 0) {
+    return (-1);
+  } else if (num === 0) {
+    return (1);
+  } else {
+    return (num * factorialize(num - 1));
+  }
+}
+console.log(factorialize(parseInt(process.argv[2])));
