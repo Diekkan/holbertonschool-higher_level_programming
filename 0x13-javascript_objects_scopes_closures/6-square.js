9515b49cff2ab7d910e3bab44559663c1df559d1
Diekkan <merentiel.diego@gmail.com>
2022-04-05 18:28:33 -0700

    added charPrint method to SQR class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,15 +4,13 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    if (size > 0) {
-      this.size = size;
     }
   }
 
   charPrint (c) {
     if (c && isNaN(c)) {
-      for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
       }
     } else if (c === undefined) {
       this.print();
