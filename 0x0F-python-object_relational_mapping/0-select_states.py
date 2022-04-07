25d212ff3f17e07d7be1e6462df16f81db1abe89
Diekkan <3950@holbertonschool.com>
2022-04-07 07:47:08 -0700

    a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+MySQLdb script that lists all states from dB.
+"""
+
+import MySQLdb
+from sys import argv
+
+if len(argv) > 3:
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    # Print results in comma delimited format
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+    cur.close()
+    db.close()
