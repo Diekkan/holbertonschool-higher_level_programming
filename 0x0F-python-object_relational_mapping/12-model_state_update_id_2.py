02738ba838fbda500cba34a04f43a68a29fbcc5a
Diekkan <3950@holbertonschool.com>
2022-04-09 13:26:28 -0700

    a script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -5,7 +5,6 @@ containing the letter a from the database hbtn_0e_6_usa
 """
 
 from model_state import Base, State
-from sqlalchemy.sql import select
 from sqlalchemy import create_engine, update
 from sqlalchemy.orm import sessionmaker
 from sys import argv
@@ -13,7 +12,7 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2],
-                                   argv[3]), pool_pre_ping=True)
+                                   argv[3]))
     conn = engine.connect()
     updt = update(State).where(State.id == 2).values(name='New Mexico')
-    result = conn.execute(updt)
+    conn.execute(updt)
