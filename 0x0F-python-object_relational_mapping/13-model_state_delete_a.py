6854689ac0423b2dc92aef2b5a69c8247b0a0392
Diekkan <3950@holbertonschool.com>
2022-04-09 12:55:39 -0700

    a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-a script that deletes all State objects with a name 
+a script that deletes all State objects with a name
 containing the letter a from the database hbtn_0e_6_usa
 """
 
