a674c60cfc4d7ef466978fdd140fdf517b54a15c
Diekkan <3950@holbertonschool.com>
2022-04-09 13:24:16 -0700

    10th ex

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,12 +13,14 @@ from sys import argv
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2],
-                                   argv[3]), pool_pre_ping=True)
-    session = sessionmaker(bind=engine)
+                                   argv[3]))
+    Session = sessionmaker(bind=engine)
     session = Session()
+
     lol = session.query(State).filter(State.name.like(argv[4])).first()
     if lol:
-        print(lol.id)
+        print(f'{lol.id}')
     else:
         print('Not found')
+
     session.close()
