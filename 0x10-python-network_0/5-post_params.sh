4cf1bda7226503af7d55d5b948f8e62ba326d3d3
Diekkan <merentiel.diego@gmail.com>
2022-04-22 16:58:54 +0000

    a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -d 'email=test@gmail.com&subject=I will always be here for PLD' -X POST $1
+curl -s -d 'email=test@gmail.com&subject=I will always be here for PLD' -X POST $1
